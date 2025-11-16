3b7585f75547996e7325889de1e587b9cafcf155
asoft rai <racshanraim555@gmail.com>
2025-11-16 16:54:43 +1100

    feat(redux): add Redux store and wrap app with Provider
    - Create  with , , ,  and
    - Add placeholder  to combined reducers and export default .
    - Wrap app in  and import  in  so Redux state is available app-wide.

---
+++ asoft-eshop/Redux/store.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducer, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
+const reducers = combineReducer({
+    // cartReducer
+
+})
+
+const store = createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
+)
+
+export default store;
