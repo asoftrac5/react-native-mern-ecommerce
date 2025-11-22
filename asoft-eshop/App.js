585ce0e7dcd9073c5f9e5ddbd2e6375220129abc
asoft rai <racshanraim555@gmail.com>
2025-11-22 12:51:35 +1100

    Fixed the conflict issue

--- asoft-eshop/App.js
+++ asoft-eshop/App.js
@@ -23,25 +23,28 @@
 //   },
 // });
 
-// App.js
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import store from './Redux/store';
+import Toast from 'react-native-toast-message';
 
-import ProductContainer from './Screens/Products/ProductContainer';
-import Header from './Screens/Shared/Header';
-
-import { GluestackUIProvider } from '@gluestack-ui/themed';
-import { config } from './gluestack-ui.config';
+import Header from './Shared/Header';
+import Main from './Navigators/Main';
 
 export default function App() {
   return (
-    <GluestackUIProvider config={config}>
-      <View style={styles.container}>
-        <Header />
-        <ProductContainer />
-        <StatusBar style="auto" />
-      </View>
-    </GluestackUIProvider>
+    <Provider store={store}>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Header />
+          <Main />
+          <StatusBar style="auto" />
+        </View>
+        <Toast />
+      </NavigationContainer>
+    </Provider>
   );
 }
 
