951065b3f28f66b0416b703a5fc71389ec4b6756
asoft rai <racshanraim555@gmail.com>
2025-11-23 01:11:50 +1100

    feat: implement auth Context API store
    - Create Auth provider with useReducer and Context
    - Auto-restore session from AsyncStorage JWT
    - Wrap app for global auth state access
    - Enable persistent authentication

--- asoft-eshop/App.js
+++ asoft-eshop/App.js
@@ -1,31 +1,34 @@
 // App.js
-import React from 'react';
-import { LogBox } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { LogBox } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 // Redux
-import { Provider } from 'react-redux';
-import store from './Redux/store';
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 
+//Context API
+import Auth from "./Context/Store/Auth";
 
 // Navigators
-import Main from './Navigators/Main';
+import Main from "./Navigators/Main";
 
-import Header from './Shared/Header';
-import ProductContainer from './Screens/Products/ProductContainer';
-import Toast from 'react-native-toast-message';
+import Header from "./Shared/Header";
+import ProductContainer from "./Screens/Products/ProductContainer";
+import Toast from "react-native-toast-message";
 
 LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Header />
-        <Main />
-        <Toast ref={(ref) => Toast.setRef(ref)} />
-      </NavigationContainer>
-    </Provider>
+    <Auth>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Header />
+          <Main />
+          <Toast ref={(ref) => Toast.setRef(ref)} />
+        </NavigationContainer>
+      </Provider>
+    </Auth>
   );
 }
-
